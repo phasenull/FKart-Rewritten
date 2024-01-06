fcd2655b38ad3566f9ff10f4524b64c184883659
phasenull <55253035+phasenull@users.noreply.github.com>
2024-01-07 00:43:22 +0300

    axios and react-query

--- src/AppEntry.tsx
+++ src/AppEntry.tsx
@@ -1,30 +1,58 @@
-import { LogBox, Text, View, useColorScheme } from "react-native"
+import {
+	LogBox,
+	Text,
+	View,
+	useColorScheme,
+} from "react-native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { NavigationContainer } from "@react-navigation/native"
-LogBox.ignoreLogs(["Non-serializable values were found in the navigation state."])
+import HomePage from "./screens/Home"
+import AuthPage from "./screens/Auth"
+import CardDetails from "./screens/CardDetails"
+import {
+	QueryClient,
+	QueryClientProvider,
+} from "react-query"
+LogBox.ignoreLogs([
+	"Non-serializable values were found in the navigation state.",
+])
 const Stack = createNativeStackNavigator()
-let Application
+import Application from "./common/Application"
+const queryClient = new QueryClient()
 export default function AppEntryComponent() {
-	LogBox.ignoreLogs(["Non-serializable values were found in the navigation state."])
+	LogBox.ignoreLogs([
+		"Non-serializable values were found in the navigation state.",
+	])
 	const colorScheme = useColorScheme()
 	console.log(colorScheme)
 	LogBox.ignoreLogs(["Require cycle:"])
-	Application = require("./util/Application").default
+	
 	Application.__INIT()
-	const HomePage = require("./pages/Home").default
-	const AuthPage = require("./pages/Auth").default
-	const CardDetails = require("./pages/CardDetails").default
 	return (
-		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false, statusBarHidden: false }}>
-				<Stack.Screen name="home" component={HomePage} />
-				<Stack.Screen name="auth" component={AuthPage} />
-				<Stack.Screen
-					name="card_details"
-					options={{ headerShown: true, headerTransparent: true, headerTitleAlign: "center", headerTitleStyle: { color: "white" }, headerTintColor: "white" }}
-					component={CardDetails}
-				/>
-			</Stack.Navigator>
-		</NavigationContainer>
+		<QueryClientProvider client={queryClient}>
+			<NavigationContainer>
+				<Stack.Navigator
+					initialRouteName="Home"
+					screenOptions={{
+						headerShown: false,
+						statusBarHidden: false,
+					}}
+				>
+					<Stack.Screen name="home" component={HomePage} />
+					<Stack.Screen name="auth" component={AuthPage} />
+					<Stack.Screen
+						name="card_details"
+						options={{
+							headerShown: true,
+							headerTransparent: true,
+							headerTitleAlign: "center",
+							headerTitleStyle: { color: "white" },
+							headerTintColor: "white",
+						}}
+						component={CardDetails}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</QueryClientProvider>
 	)
 }
