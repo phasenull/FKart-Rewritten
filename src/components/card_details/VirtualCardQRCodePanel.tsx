eb85f25bf9f5e07d1eb1f42ceea4dd5b04e23e59
phasenull <55253035+phasenull@users.noreply.github.com>
2024-01-26 22:59:29 +0300

    Separated components

---
+++ src/components/card_details/VirtualCardQRCodePanel.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useMemo, useState } from "react"
+import { Image, Text, View } from "react-native"
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated"
+import Application from "../../common/Application"
+import { BasicCardData } from "../../common/interfaces/BasicCardData"
+import { Favorite } from "../../common/enums/Favorites"
+import { getQRCode } from "../../common/hooks/useGetQRCode"
+
+export default function VirtualCardQRCodePanel(props: { card: BasicCardData<"Basic" | "QR">; token?: { expireDate: string; token: string; aliasNo: string } }) {
+	const styles = Application.styles
+	const { card, token } = props
+	const linear = useSharedValue(100)
+	const image_url = getQRCode(token?.token)
+
+	const animatedChanged = useAnimatedStyle(() => ({
+		width: `${linear.value}%`,
+	}))
+	return useMemo(() => {
+		console.log("VirtualCardQRCodePanel render")
+		if (!card || !token || !card.virtualCard) {
+			return null
+		}
+		linear.value = 100
+		linear.value = withTiming(0, { duration: Application.sync_interval, easing: Easing.linear })
+		return (
+			<View
+				className="flex-col my-4 w-80 overflow-hidden"
+				style={{
+					backgroundColor: styles.white,
+					borderRadius: 16,
+					elevation: 2,
+				}}
+			>
+				<Text style={{ color: styles.secondary, fontSize: 32, fontWeight: "600" }} className="flex-1 my-2 text-center">
+					Ücret: {card.paxDescription}
+				</Text>
+				<Image
+					className="self-center"
+					style={{
+						height: 64 * 4,
+						width: 64 * 4,
+						marginBottom: 4 * 4,
+					}}
+					source={{ uri: image_url }}
+				/>
+				<Animated.View style={[animatedChanged, { backgroundColor: styles.primary, height: 16 }]} />
+			</View>
+		)
+	}, [card, token])
+}
--- src/screens/CardDetails.tsx
+++ src/screens/CardDetails.tsx
@@ -17,6 +17,9 @@ import { useGetSyncCode } from "../common/hooks/useGetSyncCode"
 import { getQRCode } from "../common/hooks/useGetQRCode"
 import CustomLoadingIndicator from "../components/CustomLoadingIndicator"
 import SelectCardTypeModal from "../components/card_details/SelectCardTypeModal"
+import CardDetailsHeader from "../components/card_details/CardDetailsHeader"
+import CardJSONData from "../components/card_details/CardJSONData"
+import VirtualCardQRCodePanel from "../components/card_details/VirtualCardQRCodePanel"
 export default function CardDetails(props: {
 	route: {
 		params: {
@@ -32,15 +35,16 @@ export default function CardDetails(props: {
 	const favorite_data = props?.route.params?.favorite_data
 	const card = props?.route.params?.card
 	const is_virtual = props?.route.params?.is_virtual
-	const { data: syncData } = useGetSyncCode(is_virtual ? card?.aliasNo || favorite_data?.favorite : undefined)
+	let syncData: undefined | { data: { cardInfo: { expireDate: string; token: string; aliasNo: string } } } = undefined
+	if (is_virtual) {
+		syncData = useGetSyncCode(card?.aliasNo || favorite_data?.favorite).data
+	}
 	const [cardToken, setCardToken] = useState<undefined | { expireDate: string; token: string; aliasNo: string }>(undefined)
 	const { navigation } = props
-	const [card_image, setCardImage] = useState(CardImages.undefined)
 	const [card_type, setCardType] = useState(CardTypes.undefined)
 	const [showEditCardTypeModal, setShowEditCardTypeModal] = useState(false)
 	useEffect(() => {
 		async function get() {
-			setCardImage(await Card.getImageFromCard({ ...card, ...favorite_data }))
 			setCardType(await Card.getTypeFromCard({ ...card, ...favorite_data }))
 		}
 		get()
@@ -53,15 +57,12 @@ export default function CardDetails(props: {
 			},
 		})
 	}, [card_type])
-	const linear = useSharedValue(100)
-	const animatedChanged = useAnimatedStyle(() => ({
-		width: `${linear.value}%`,
-	}))
 	useEffect(() => {
+		if (!syncData || !syncData.data) {
+			return
+		}
 		if (syncData?.data?.cardInfo?.token) {
 			setCardToken(syncData?.data.cardInfo)
-			linear.value = 100
-			linear.value = withTiming(0, { duration: 5000, easing: Easing.linear })
 		}
 	}, [syncData?.data])
 	if (!card || !favorite_data) {
@@ -71,7 +72,6 @@ export default function CardDetails(props: {
 			</View>
 		)
 	}
-
 	return (
 		<Animated.ScrollView
 			// tofix
@@ -83,108 +83,21 @@ export default function CardDetails(props: {
 				visible={showEditCardTypeModal}
 				onDismiss={() => setShowEditCardTypeModal(false)}
 				defaultValue={card_type}
-				onSelect={async (value) => {
+				onSelect={(value) => {
 					setShowEditCardTypeModal(false)
 					setCardType(value)
-					await Application.database.set("card__" + card.aliasNo, value)
+					Application.database.set("card__" + card.aliasNo, value)
 				}}
 			/>
-			<LinearGradient colors={[styles.primary, styles.white]} end={{ x: 0.8, y: 0.9 }}  className="flex-row h-56 w-full mb-20" style={{ backgroundColor: styles.primary }}>
-				<Animated.View entering={FadeInLeft.duration(500)} className="flex-col flex-1 ml-5 self-end mb-4 ">
-					<Text className="opacity-70 top-3 text-white text-xl">Bakiye</Text>
-					<View className="flex-row items-baseline text-white gap-x-2 mb-4">
-						<Text className="font-bold text-white text-[48px]">{card.balance}</Text>
-						<Text className="text-[28px] text-white font-bold">TL</Text>
-					</View>
-					<TouchableOpacity
-						onPress={() => {
-							Clipboard.setString(card.aliasNo)
-							ToastAndroid.show("Kart numarası kopyalandı!", ToastAndroid.SHORT)
-							// vibrate device
-							Vibration.vibrate(100)
-						}}
-						onLongPress={() => {
-							Clipboard.setString(card.aliasNo)
-							ToastAndroid.show("Kart numarası kopyalandı!", ToastAndroid.SHORT)
-							// vibrate device
-							Vibration.vibrate(100)
-						}}
-						className="flex-row w-full gap-x-1 items-center"
-					>
-						<View className="flex-col">
-							<Text className="opacity-70 text-white text-xl">Kart Numarası</Text>
-							<Text className="font-bold bottom-1 text-white text-xl">{card.aliasNo}</Text>
-						</View>
-						<MaterialCommunityIcons style={{ color: styles.white }} size={20} name="content-copy" />
-					</TouchableOpacity>
-				</Animated.View>
-				<Animated.View entering={FadeInDown.duration(500)} className="flex-1 mr-24">
-					<TouchableOpacity disabled={card_type === "QR"} onPress={() => setShowEditCardTypeModal(true)} className="relative w-40 h-64 left-16 -bottom-8 ">
-						<Image
-							className={"w-64 h-64 right-14"}
-							style={{
-								transform: [{ rotateZ: "90deg" }],
-								objectFit: "contain",
-							}}
-							source={{
-								uri: card_image,
-							}}
-						/>
-						{card_type === "QR" ? null : <MaterialCommunityIcons style={{ bottom: 16 * 4, right: 4 * 4, alignSelf: "flex-end" }} color={styles.dark} name="image-edit" size={36} />}
-					</TouchableOpacity>
-				</Animated.View>
-			</LinearGradient>
-			{/* <View>
-				<Text style={{ color: styles.secondary }} className="mt-2 font-bold text-center text-[36px]">
-					{card.balance} TL
-					{card.loads_in_line ? (
-						<View className="absolute w-6 h-6 justify-center  -top-1.5 self-start rounded-full bg-red-400">
-							<Text className="text-white text-center text-xl font-bold">{card.loads_in_line?.length || "??"}</Text>
-						</View>
-					) : null}
-				</Text>
-				<Text style={{ color: styles.secondary }} className="opacity-20 mb-2 font-bold text-center text-xl">
-					Tip: "{card.card_type || "undefined"}"
-				</Text>
-			</View> */}
+			<CardDetailsHeader card={card} card_type={card_type} setShowEditCardTypeModal={setShowEditCardTypeModal} />
+
 			<CardControlPanel makeRefresh={() => {}} card={card} favorite_data={favorite_data} navigation={navigation} is_virtual={is_virtual} />
 
-			{card.virtualCard === "1" && cardToken ? (
-				<View
-					className="flex-col my-4 w-80 overflow-hidden"
-					style={{
-						backgroundColor: styles.white,
-						borderRadius: 16,
-						elevation: 2,
-					}}
-				>
-					<Text style={{ color: styles.secondary, fontSize: 32, fontWeight: "600" }} className="flex-1 my-2 text-center">
-						Ücret: {card.paxDescription}
-					</Text>
-					<Image
-						className="self-center"
-						style={{
-							height: 64 * 4,
-							width: 64 * 4,
-							marginBottom: 4 * 4,
-						}}
-						source={{ uri: getQRCode(cardToken.token) }}
-					/>
-					<Animated.View style={[animatedChanged, { backgroundColor: styles.primary, height: 16 }]} />
-				</View>
-			) : null}
-			<Text
-				className="p-4 my-10 w-80"
-				style={{
-					backgroundColor: styles.dark,
-					borderRadius: 16,
-					elevation: 10,
-					color: styles.secondaryDark,
-				}}
-			>
-				Card Data: {JSON.stringify({ ...favorite_data, ...card }, null, 4)}
-				{/* Loads in line: {JSON.stringify(card.loads_in_line, null, 4)} */}
-			</Text>
+			{/* {card.virtualCard === "1" && cardToken ? (
+				
+			) : null} */}
+			<VirtualCardQRCodePanel card={card} token={cardToken} />
+			<CardJSONData card={card} favorite_data={favorite_data} />
 		</Animated.ScrollView>
 	)
 }
