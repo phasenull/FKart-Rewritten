ac05c8024fd16057ed2fb93c094e87073c30c025
phasenull <55253035+phasenull@users.noreply.github.com>
2024-04-10 13:03:28 +0300

    new formatter,config props for InputModal

--- src/components/InputModal.tsx
+++ src/components/InputModal.tsx
@@ -1,10 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Application from "../common/Application"
 import { Modal, Text, TextInput, TouchableOpacity, View } from "react-native"
+import { formatAlias } from "../util"
 
-export default function InputModal(props: { visible?: boolean; onDismiss: () => void; onSave?: (text: string) => void; defaultValue?:string }) {
+export default function InputModal(props: {
+	visible?: boolean
+	onDismiss: () => void
+	onSave?: (text: string) => void
+	defaultValue?: string
+	formatter?: (input: string) => string
+	config?: { confirm: string; cancel: string; title: string; placeholder?: string }
+}) {
 	const styles = Application.styles
 	const [input, setInput] = useState(props.defaultValue || "")
+	function formatInput(input:string) {
+		const result = props.formatter ? props.formatter(input) || input : input
+		return result
+	}
 	const { onSave, onDismiss } = props
 	if (!props.visible) {
 		return null
@@ -13,18 +25,19 @@ export default function InputModal(props: { visible?: boolean; onDismiss: () => 
 		<Modal transparent={true} onDismiss={props.onDismiss}>
 			<View className="p-4 w-80 self-center my-auto rounded-[16px] flex-col items-center justify-center" style={{ backgroundColor: styles.dark, elevation: 10 }}>
 				<Text className="text-center" style={{ color: styles.secondary, fontSize: 18 }}>
-					Rename Card
+					{props.config?.title || "unknown_key (config.description)"}
 				</Text>
 				<TextInput
 					onChangeText={setInput}
+					value={formatInput(input)}
 					defaultValue={props.defaultValue}
 					className="w-full h-16 my-4 px-4"
-					placeholder="My Favorite Card"
+					placeholder={props.config?.placeholder || "Input your text here"}
 					style={{ borderColor: styles.secondary, borderWidth: 2, borderRadius: 8, color: styles.secondary, fontSize: 18, fontWeight: "500" }}
 				/>
 				<View className="flex-row justify-center items-center gap-x-4">
 					<TouchableOpacity className="rounded-[12px] items-center justify-center h-12 px-4" onPress={onDismiss} style={{ backgroundColor: styles.warning }}>
-						<Text style={{ color: styles.secondary, fontSize: 18, fontWeight: "600" }}>Cancel</Text>
+						<Text style={{ color: styles.secondary, fontSize: 18, fontWeight: "600" }}>{props.config?.cancel || "Cancel"}</Text>
 					</TouchableOpacity>
 					<TouchableOpacity
 						className="rounded-[12px] items-center justify-center h-12 px-4"
@@ -37,7 +50,7 @@ export default function InputModal(props: { visible?: boolean; onDismiss: () => 
 						}}
 						style={{ backgroundColor: styles.success }}
 					>
-						<Text style={{ color: styles.secondary, fontSize: 18, fontWeight: "600" }}>Save</Text>
+						<Text style={{ color: styles.secondary, fontSize: 18, fontWeight: "600" }}>{props.config?.confirm || "OK"}</Text>
 					</TouchableOpacity>
 				</View>
 			</View>
