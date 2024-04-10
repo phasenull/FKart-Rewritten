8b92c9158bbaa8d9e92ef8529ec35817ca8c5130
phasenull <55253035+phasenull@users.noreply.github.com>
2024-04-10 13:02:54 +0300

    util.formatAlias fix

--- src/util.ts
+++ src/util.ts
@@ -12,6 +12,6 @@ export function hidePhone(phone: string | undefined) {
 export function formatAlias(alias:string | undefined) {
 	const part1 : string = alias?.slice(0,5) as string
 	const part2 : string = alias?.slice(5,10) as string
-	const part3 : string = alias?.slice(11,12) as string
+	const part3 : string = alias?.slice(10,11) as string
 	return `${part1 + "X".repeat(5-(part1?.length as number))}-${part2 + "X".repeat(5-(part2?.length as number))}-${part3 + "X".repeat(1-(part3?.length as number))}`
 }
