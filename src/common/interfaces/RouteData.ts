8608bd6a97a0daede1014dbeba1f64808468348f
phasenull <55253035+phasenull@users.noreply.github.com>
2024-04-10 13:29:29 +0300

    RouteData added timeTableList

--- src/common/interfaces/RouteData.ts
+++ src/common/interfaces/RouteData.ts
@@ -1,6 +1,6 @@
-import BasicStopInformation from "./BasicStopInformation";
-import BusData from "./BusData";
-import IPoint from "./Point";
+import BasicStopInformation from "./BasicStopInformation"
+import BusData from "./BusData"
+import IPoint from "./Point"
 
 export default interface RouteData extends Object {
 	busList: BusData[]
@@ -12,7 +12,13 @@ export default interface RouteData extends Object {
 	path_code: string
 	pointList: IPoint[]
 	scheduleList: any
-	stopTimeList: any,
-	timeTableList: any
+	stopTimeList: any
+	timeTableList: {
+		departureTime: string
+		wheelChairId: "0"
+		tripId: string
+		patternColor: string
+		tripHeadSign: string
+	}[]
 	tripShortName: string
 }
