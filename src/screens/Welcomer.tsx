406523c6e6ce5c53277ccebf3d1b8d00a355fd27
phasenull <55253035+phasenull@users.noreply.github.com>
2024-04-11 14:20:12 +0300

    Updates Welcomer part

--- src/screens/Welcomer.tsx
+++ src/screens/Welcomer.tsx
@@ -1,10 +1,52 @@
 import { useState } from "react"
 import { InitialInfo } from "../components/welcomer/InitialInfo"
-
-export default function WelcomerPage() {
+import { NativeStackNavigationProp } from "@react-navigation/native-stack"
+import { Text, View } from "react-native"
+import Application from "../common/Application"
+import CustomLoadingIndicator from "../components/CustomLoadingIndicator"
+import * as Updates from 'expo-updates';
+export default function WelcomerPage(props: { route: any }) {
+	const { isCheckingUpdate, isUpdateAvailable } = props.route.params
+	console.log(isCheckingUpdate, isUpdateAvailable)
 	const [show, setShow] = useState<"initial_info" | "auth_page">("initial_info")
-
+	if (isCheckingUpdate) {
+		return (
+			<View className="flex-1 gap-y-10 flex-col items-center justify-center" style={{ backgroundColor: Application.styles.dark }}>
+				<View>
+					<CustomLoadingIndicator size={64} />
+				</View>
+				<Text
+					// className="bg-red-400"
+					style={{
+						color: Application.styles.secondary,
+						fontWeight: "800",
+						fontSize:24
+					}}
+				>
+					Checking Updates
+				</Text>
+			</View>
+		)
+	}if (isUpdateAvailable) {
+		return (
+			<View className="flex-1 gap-y-10 flex-col items-center justify-center" style={{ backgroundColor: Application.styles.dark }}>
+				<View>
+					<CustomLoadingIndicator size={64} />
+				</View>
+				<Text
+					// className="bg-red-400"
+					style={{
+						color: Application.styles.secondary,
+						fontWeight: "800",
+						fontSize:24
+					}}
+				>
+					Updating to {Updates.createdAt?.getUTCDate()}
+				</Text>
+			</View>
+		)
+	}
 	if (show == "initial_info") {
-		return <InitialInfo/>
+		return <InitialInfo />
 	}
 }
