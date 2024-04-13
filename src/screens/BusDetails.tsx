27bf485725dc7068edf5289d1b3107701582ef64
phasenull <55253035+phasenull@users.noreply.github.com>
2024-04-13 22:24:29 +0300

    syntax format

--- src/screens/BusDetails.tsx
+++ src/screens/BusDetails.tsx
@@ -1,19 +1,5 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack"
-import {
-	Button,
-	Clipboard,
-	Image,
-	Linking,
-	RefreshControl,
-	ScrollView,
-	Text,
-	TextInput,
-	ToastAndroid,
-	TouchableOpacity,
-	TouchableWithoutFeedback,
-	Vibration,
-	View,
-} from "react-native"
+import { Button, Clipboard, Image, Linking, RefreshControl, ScrollView, Text, TextInput, ToastAndroid, TouchableOpacity, TouchableWithoutFeedback, Vibration, View } from "react-native"
 import Application from "../common/Application"
 import React, { useEffect, useState } from "react"
 import BusData from "../common/interfaces/KentKart/object/BusData"
@@ -24,28 +10,16 @@ import * as ImagePicker from "expo-image-picker"
 import { putBusImages } from "../common/hooks/fkart/bus/usePutBusImages"
 import { Buffer } from "buffer"
 import Animated from "react-native-reanimated"
-export default function BusDetails(props: {
-	route: { params: { bus: BusData } }
-	navigation: NativeStackNavigationProp<any>
-}) {
+export default function BusDetails(props: { route: { params: { bus: BusData } }; navigation: NativeStackNavigationProp<any> }) {
 	const styles = Application.styles
 	const [loading, setLoading] = useState(false)
 	const { navigation } = props
 	const { bus } = props.route.params
-	const {
-		data,
-		isError,
-		isLoading,
-		isRefetching,
-		error,
-		refetch,
-	} = useGetBusImages(bus)
+	const { data, isError, isLoading, isRefetching, error, refetch } = useGetBusImages(bus)
 
 	useEffect(() => {
 		navigation.setOptions({
-			headerTitle: `${
-				props.route?.params?.bus?.plateNumber || "unnamed bus"
-			}`,
+			headerTitle: `${props.route?.params?.bus?.plateNumber || "unnamed bus"}`,
 			headerTintColor: styles.secondary,
 			headerTitleAlign: "left",
 			headerTitleStyle: {
@@ -60,9 +34,7 @@ export default function BusDetails(props: {
 			setToken(token)
 		})()
 	}, [])
-	const [image, setImage] = useState<string | undefined>(
-		undefined
-	)
+	const [image, setImage] = useState<string | undefined>(undefined)
 
 	const pickImage = async () => {
 		// No permissions request is necessary for launching the image library
@@ -112,9 +84,7 @@ export default function BusDetails(props: {
 	if (!props?.route?.params?.bus) {
 		return (
 			<View className="flex-1 items-center justify-center">
-				<Text style={{ color: styles.warning, fontSize: 24 }}>
-					No card data found
-				</Text>
+				<Text style={{ color: styles.warning, fontSize: 24 }}>No card data found</Text>
 			</View>
 		)
 	}
@@ -125,26 +95,15 @@ export default function BusDetails(props: {
 		return (
 			<Animated.ScrollView
 				// tofix
-				refreshControl={
-					<RefreshControl
-						refreshing={loading}
-						onRefresh={() => refetch()}
-					/>
-				}
+				refreshControl={<RefreshControl refreshing={loading} onRefresh={() => refetch()} />}
 				className="flex-col"
 				contentContainerStyle={{
 					alignItems: "center",
 					padding: 20,
 				}}
 			>
-				<Text style={{ color: styles.warning, fontSize: 24 }}>
-					{(error as {"message":string})?.message}
-				</Text>
-				<TextInput
-					onChangeText={setToken}
-					placeholder={token}
-					className="bg-slate-500 w-80 h-20 text-center"
-				></TextInput>
+				<Text style={{ color: styles.warning, fontSize: 24 }}>{(error as { message: string })?.message}</Text>
+				<TextInput onChangeText={setToken} placeholder={token} className="bg-slate-500 w-80 h-20 text-center"></TextInput>
 				<TouchableOpacity
 					className="bg-red-400 w-16 h-16"
 					onPress={async () => {
@@ -158,9 +117,7 @@ export default function BusDetails(props: {
 	return (
 		<Animated.ScrollView
 			// tofix
-			refreshControl={
-				<RefreshControl refreshing={loading} onRefresh={refetch} />
-			}
+			refreshControl={<RefreshControl refreshing={loading} onRefresh={refetch} />}
 			className="flex-col gap-y-5"
 			contentContainerStyle={{
 				alignItems: "center",
@@ -189,10 +146,7 @@ export default function BusDetails(props: {
 				}}
 				className="items-center py-4 w-80 flex-col gap-y-5 overflow-hidden"
 			>
-				<Button
-					title="Pick an image from camera roll"
-					onPress={pickImage}
-				/>
+				<Button title="Pick an image from camera roll" onPress={pickImage} />
 				{image && (
 					<React.Fragment>
 						<Image className="w-80 h-36" source={{ uri: image }} />
@@ -212,45 +166,38 @@ export default function BusDetails(props: {
 			>
 				<Text>{JSON.stringify(bus, undefined, 4)}</Text>
 			</View>
-			{data?.data.data?.map(
-				(image_data: {
-					url: string
-					uploadedAt: string
-					size: number
-				}) => (
-					<TouchableOpacity
-						onPress={() => {
-							Linking.openURL(image_data.url)
+			{data?.data.data?.map((image_data: { url: string; uploadedAt: string; size: number }) => (
+				<TouchableOpacity
+					onPress={() => {
+						Linking.openURL(image_data.url)
+					}}
+					onLongPress={() => {
+						Clipboard.setString(bus.plateNumber)
+						ToastAndroid.show("Copied to clipboard!", ToastAndroid.SHORT)
+						Vibration.vibrate(100)
+					}}
+					key={`${image_data.url}`}
+					style={{
+						backgroundColor: Application.styles.white,
+						borderRadius: 16,
+						elevation: 10,
+						shadowOffset: { height: 4, width: 4 },
+					}}
+					className="h-max w-80 flex-col overflow-hidden items-center"
+				>
+					<Text>
+						{image_data.uploadedAt} {image_data.size}
+					</Text>
+					<Image
+						source={{
+							uri: image_data.url,
+							// cache: "force-cache",
 						}}
-						onLongPress={()=>{
-							Clipboard.setString(bus.plateNumber)
-							ToastAndroid.show("Copied to clipboard!",ToastAndroid.SHORT)
-							Vibration.vibrate(100)
-						}}
-						key={`${image_data.url}`}
-						style={{
-							backgroundColor: Application.styles.white,
-							borderRadius: 16,
-							elevation: 10,
-							shadowOffset: { height: 4, width: 4 },
-						}}
-						className="h-max w-80 flex-col overflow-hidden items-center"
-					>
-						<Text>
-							{image_data.uploadedAt} {image_data.size}
-						</Text>
-						<Image
-							source={{
-								uri: image_data.url,
-								cache: "force-cache",
-							}}
-							className="w-80 h-80"
-							style={{ objectFit: "contain" }}
-						/>
-						<Text>{image_data.url}</Text>
-					</TouchableOpacity>
-				)
-			)}
+						style={{ objectFit: "contain", width: 80 * 4, height: 80*4 }}
+					/>
+					<Text>{image_data.url}</Text>
+				</TouchableOpacity>
+			))}
 		</Animated.ScrollView>
 	)
 }
