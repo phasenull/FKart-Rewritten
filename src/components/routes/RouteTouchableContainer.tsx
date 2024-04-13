f1d218b56ec7ad81f468b0279f4e4121257cef6c
phasenull <55253035+phasenull@users.noreply.github.com>
2024-04-13 22:23:40 +0300

    source format

--- src/components/routes/RouteTouchableContainer.tsx
+++ src/components/routes/RouteTouchableContainer.tsx
@@ -12,38 +12,42 @@ export default function RouteTouchableContainer(props: { navigation: NativeStack
 
 	return useMemo(
 		() => (
-			
-				<TouchableOpacity onPress={() => navigation.navigate("route_details", { data_route: item })} className="py-1 flex-row pl-1">
-					<View className="flex-[0.22] my-auto items-center ">
-						<View
-							className="px-2 h-6"
-							style={{
-								backgroundColor: item.routeColor === "006633" ? Application.styles.primary : `#${item.routeColor}`,
-								borderRadius: 6,
-							}}
-						>
-							<Text
-								className="text-center my-auto"
-								style={{
-									color: item.routeColor === "006633" ? Application.styles.secondary : `#${item.routeTextColor}`,
-									fontWeight: "900",
-								}}
-							>
-								{item.displayRouteCode}
-							</Text>
-						</View>
-					</View>
-					<Divider className="ml-0.5 mr-0.5 self-center" />
-					<Text
-						className="my-auto ml-1 mr-4 flex-1"
+			<TouchableOpacity
+				onPress={() => {
+					navigation.push("route_details", { data_route: item })
+				}}
+				className="py-1 flex-row pl-1"
+			>
+				<View className="flex-[0.22] my-auto items-center ">
+					<View
+						className="px-2 h-6"
 						style={{
-							color: Application.styles.secondary,
-							fontWeight: "500",
+							backgroundColor: item.routeColor === "006633" ? Application.styles.primary : `#${item.routeColor}`,
+							borderRadius: 6,
 						}}
 					>
-						{item.name} ({item.routeCode})
-					</Text>
-				</TouchableOpacity>
+						<Text
+							className="text-center my-auto"
+							style={{
+								color: item.routeColor === "006633" ? Application.styles.secondary : `#${item.routeTextColor}`,
+								fontWeight: "900",
+							}}
+						>
+							{item.displayRouteCode}
+						</Text>
+					</View>
+				</View>
+				<Divider className="ml-0.5 mr-0.5 self-center" />
+				<Text
+					className="my-auto ml-1 mr-4 flex-1"
+					style={{
+						color: Application.styles.secondary,
+						fontWeight: "500",
+					}}
+				>
+					{item.name} ({item.routeCode})
+				</Text>
+			</TouchableOpacity>
 		),
 		[item]
 	)
