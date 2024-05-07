653e602c07167ec041cfe30be3f45d3f49edd411
phasenull <55253035+phasenull@users.noreply.github.com>
2024-05-07 18:33:19 +0300

    added Database.removeItem

--- src/common/classes/Database.ts
+++ src/common/classes/Database.ts
@@ -1,26 +1,32 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import Logger from "../Logger";
-
-
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import Logger from "../Logger"
 
 export default abstract class Database {
 	public static async get(key: string) {
 		try {
-			const data= await AsyncStorage.getItem(key) as string
+			const data = (await AsyncStorage.getItem(key)) as string
 			if (!data) return null
 			const jsondata = JSON.parse(data)
 
 			return jsondata || null
-		} catch (e : string | any) {
+		} catch (e: string | any) {
 			// Logger.warning("Database.get",e)
 			return null
 		}
 	}
-	public static async set(key: string, value: Map<any,any>|Array<any>|string|boolean|number|undefined|null) {
+	public static async set(key: string, value: Map<any, any> | Array<any> | string | boolean | number | undefined | null) {
 		try {
 			await AsyncStorage.setItem(key, JSON.stringify(value))
-		} catch (e : string | any) {
-			Logger.error("Database.set",e)
+		} catch (e: string | any) {
+			Logger.error("Database.set", e)
+		}
+	}
+
+	public static async removeItem(key: string) {
+		try {
+			await AsyncStorage.removeItem(key)
+		} catch (e: string | any) {
+			Logger.error("Database.removeItem",e)
 		}
 	}
 }
