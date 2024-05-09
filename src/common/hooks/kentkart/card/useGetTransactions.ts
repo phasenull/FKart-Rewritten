b665c4746068b2bf44f13dac6f983147ae29c5e8
phasenull <55253035+phasenull@users.noreply.github.com>
2024-05-09 21:09:38 +0300

    getTransactions now requires {month,year}

--- src/common/hooks/kentkart/card/useGetTransactions.ts
+++ src/common/hooks/kentkart/card/useGetTransactions.ts
@@ -4,20 +4,15 @@ import { BaseKentKartResponse } from "../../../interfaces/KentKart/BaseKentKartR
 import { BasicCardData } from "../../../interfaces/KentKart/BasicCardData"
 import { AxiosResponse } from "axios"
 
-async function getCardTransactions(
-	card_alias: string,term:string
-): Promise<
-	AxiosResponse<BaseKentKartResponse & {}>
-> {
+async function getCardTransactions(card_alias: string, term: string): Promise<AxiosResponse<BaseKentKartResponse & {}>> {
 	// Logger.info(`REQUEST useGetCardData ${card_alias}`)
 	const url = `${Application.endpoints.service}/rl1/api/card/transaction?region=${Application.region}&lang=tr&authType=4&token=${Application.logged_user?.access_token}&alias=${card_alias}&term=${term}`
 	return Application.makeKentKartRequest(url)
 }
 
-export function useGetTransactions(card_alias: string,term:string) {
-	return useQuery(
-		["getCardData", card_alias,term],
-		() => getCardTransactions(card_alias,term),
-		{ staleTime: Infinity }
-	)
+export function useGetTransactions(card_alias: string, term: { month: number; year: number }) {
+	const stringified_month = term.month.toString().padStart(2, "0")
+	const stringified_year = term.year.toString()
+	const final_date = `${stringified_year}${stringified_month}`
+	return useQuery(["getCardData", card_alias, term], () => getCardTransactions(card_alias, final_date), { staleTime: Infinity })
 }
