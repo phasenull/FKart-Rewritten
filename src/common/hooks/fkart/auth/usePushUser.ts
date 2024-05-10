7e2b04d5c5cd30b262149f0ca93d2c91e8e14041
phasenull <55253035+phasenull@users.noreply.github.com>
2024-05-10 13:27:23 +0300

    usePushUser

---
+++ src/common/hooks/fkart/auth/usePushUser.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "react-query"
+import axios, { AxiosResponse } from "axios"
+import BaseFKartResponse from "../../../interfaces/FKart/BaseFKartResponse"
+import Logger from "../../../Logger"
+import Application from "../../../Application"
+import FKartUser from "../../../interfaces/FKart/FKartUser"
+
+async function postUser(credentials?: { password?: string; username?: string},captcha_token?: string ): Promise<
+	AxiosResponse<
+		BaseFKartResponse & {
+			user: FKartUser
+		}
+	>
+> {
+	Logger.info("REQUEST templateHook")
+	return axios(`${Application.fkart_endpoints.auth}/user/push`, {
+		method: "POST",
+		data: JSON.stringify({
+			email: credentials?.username,
+			password: credentials?.password,
+			captcha_token:captcha_token,
+		}),
+	})
+}
+
+export default function usePushUser(credentials?: { password?: string; username?: string }, captcha_token?: string) {
+	console.log(credentials,captcha_token)
+	return useQuery(["pushUser"], () => postUser(credentials,captcha_token), { enabled: false, retry: false })
+}
