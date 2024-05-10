cd83389abf5f9ae4475f395d73a7e983e8d4a9b1
phasenull <55253035+phasenull@users.noreply.github.com>
2024-05-10 22:26:19 +0300

    new UI.TextInput

--- src/screens/FKart/auth/push/PushUserPage.tsx
+++ src/screens/FKart/auth/push/PushUserPage.tsx
@@ -2,12 +2,14 @@ import { View } from "react-native"
 import Application from "../../../../common/Application"
 import Button from "../../../../components/ui/Button"
 import SecondaryText from "../../../../components/root/SecondaryText"
+import TextInput from "../../../../components/ui/TextInput"
 
 export default function PushUserPage(props: { goBack: () => void }) {
-	console.log("hello")
 	return (
 		<View className="flex-1 justify-center items-center" style={{ backgroundColor: Application.styles.dark }}>
+			<TextInput>
 
+			</TextInput>
 			<Button text="<" onPress={props.goBack} />
 		</View>
 	)
