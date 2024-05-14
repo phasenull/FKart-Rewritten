d58aa822eeb29526f6b9e9ecc478cecee6d36e63
phasenull <55253035+phasenull@users.noreply.github.com>
2024-05-14 23:21:02 +0300

    LogView custom styles

--- src/screens/FKart/editor/tab/LogsView.tsx
+++ src/screens/FKart/editor/tab/LogsView.tsx
@@ -3,19 +3,19 @@ import { getLogsAsync } from "common/hooks/fkart/auth/getLogs"
 import ICredentials from "common/interfaces/app/Credentials"
 import CustomLoadingIndicator from "components/root/CustomLoadingIndicator"
 import { useContext } from "react"
-import { Text, View } from "react-native"
+import { Text, View, ViewProps } from "react-native"
 import { useQuery } from "react-query"
 import LogTouchable from "./LogTouchable"
 import { ThemeContext } from "common/contexts/ThemeContext"
 import SecondaryText from "components/root/SecondaryText"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 
-export default function LogsView() {
+export default function LogsView(props: ViewProps) {
 	// Only use in components that are child of a FKartValidator
 	const { userManager } = useContext(FKartContext)
 	const { theme } = useContext(ThemeContext)
 	const { isFetching, data, isRefetching, isError, error } = useQuery(["getLogs"], () => getLogsAsync(userManager.accessToken), { refetchInterval: 60 * 1000, staleTime: 5 * 1000 })
-	if (isFetching &&!isRefetching) {
+	if (isFetching && !isRefetching) {
 		return <CustomLoadingIndicator size={12 * 4} />
 	}
 	if (isError) {
@@ -26,10 +26,24 @@ export default function LogsView() {
 		)
 	}
 	return (
-		<View style={{ backgroundColor: theme.white, elevation: 10, borderRadius: 16, paddingHorizontal: 4 * 4, paddingVertical: 2 * 4, width: "80%", maxWidth: 80 * 4, minWidth: 40 * 4 }}>
+		<View
+			style={[
+				{
+					backgroundColor: theme.white,
+					elevation: 10,
+					borderRadius: 16,
+					paddingHorizontal: 4 * 4,
+					paddingVertical: 2 * 4,
+					width: "80%",
+					maxWidth: 80 * 4,
+					minWidth: 40 * 4,
+				},
+				props.style,
+			]}
+		>
 			{isRefetching ? (
-				<View className="absolute self-center flex-1 top-10" >
-					<CustomLoadingIndicator size={4*4} />
+				<View className="absolute self-center flex-1 top-10">
+					<CustomLoadingIndicator size={4 * 4} />
 				</View>
 			) : null}
 			<SecondaryText className="self-center mb-2">Logs</SecondaryText>
