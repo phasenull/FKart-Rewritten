667ae1a6bb65d7f1d92bcabfcdc53f7d8e2587c1
phasenull <55253035+phasenull@users.noreply.github.com>
2024-05-15 17:38:39 +0300

    small fix

--- src/common/hooks/fkart/api/getSeasonAsync.ts
+++ src/common/hooks/fkart/api/getSeasonAsync.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 import Application from "common/Application";
 
 export default async function getSeasonAsync() {
-	return axios(`${Application.fkart_endpoints.api}/season`)
+	return axios(`${Application.fkart_endpoints.api}/season/get`)
 }
--- src/components/validators/SeasonValidator.tsx
+++ src/components/validators/SeasonValidator.tsx
@@ -1,20 +1,22 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import { AxiosResponse } from "axios"
+import Logger from "common/Logger"
 import { ThemeContext } from "common/contexts/ThemeContext"
 import getSeasonAsync from "common/hooks/fkart/api/getSeasonAsync"
 import BaseFKartResponse from "common/interfaces/FKart/BaseFKartResponse"
 import CustomLoadingIndicator from "components/root/CustomLoadingIndicator"
 import SecondaryText from "components/root/SecondaryText"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { View } from "react-native"
 import { processColorsInProps } from "react-native-reanimated/lib/typescript/reanimated2/Colors"
 import { UseQueryResult, useQuery } from "react-query"
 
-export default function SeasonValidator(props:{children:any}) {
-	const { data, error, isError, isFetching, isRefetching } = useQuery(["getSeason"], getSeasonAsync,{refetchInterval:5*60*1000,staleTime:5*60*1000,retry:2}) as UseQueryResult<
+export default function SeasonValidator(props: { children: any }) {
+	const { data, error, isError, isFetching, isRefetching } = useQuery(["getSeason"], getSeasonAsync, { refetchInterval: 5 * 60 * 1000, staleTime: 5 * 60 * 1000, retry: 2 }) as UseQueryResult<
 		AxiosResponse<
 			BaseFKartResponse & {
-				season: "summer" | "winter"
+				season: "summer" | "winter",
+				since:number
 			}
 		>
 	>
@@ -30,22 +32,24 @@ export default function SeasonValidator(props:{children:any}) {
 	if (!data?.data || isError || error) {
 		return (
 			<View className="flex-1 items-center justify-center">
-			<MaterialCommunityIcons name="snowflake-off" size={24*4} />
+				<MaterialCommunityIcons name="snowflake-off" size={24 * 4} />
 				<SecondaryText style={{ color: theme.error }}>Season service is not available, cant proceed</SecondaryText>
 			</View>
 		)
 	}
 	const season = data?.data.season
 	if (season === "winter") {
-		return <View className="flex-1 items-center justify-center">
-			<MaterialCommunityIcons name="snowflake" size={24*4} />
-			<SecondaryText style={{fontSize:36}}>
-				Winter is here!
-			</SecondaryText>
-			<SecondaryText>
-				This service is not available in winter.
-			</SecondaryText>
-		</View>
+		return (
+			<View className="flex-1 items-center justify-center">
+				<MaterialCommunityIcons color={"#abdbe3"} name="snowflake" size={24 * 4} />
+				<SecondaryText style={{ fontSize: 36 }}>Winter is here!</SecondaryText>
+				<SecondaryText style={{marginTop:4*4}}>This service is not available in winter.</SecondaryText>
+				<SecondaryText style={{opacity:0.3,fontSize:12}}>
+					Days since winter: {Math.floor(data.data.since/(24*60*60*1_000))}
+				</SecondaryText>
+			</View>
+		)
 	}
+	Logger.info("SeasonValidator","🎶It's always summer, under the sea\nI know, I know, oh, oh, oh🎶")
 	return props.children
 }
