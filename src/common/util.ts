a2b89b9cd1866d8771fdde7ff66039df3da979d9
phasenull <55253035+phasenull@users.noreply.github.com>
2024-06-13 21:41:02 +0300

    moved some functions from Application.ts to util.ts

--- src/common/util.ts
+++ src/common/util.ts
@@ -15,6 +15,30 @@ export function formatAlias(alias:string | undefined) {
 	const part3 : string = alias?.slice(10,11) as string
 	return `${part1 + "X".repeat(5-(part1?.length as number))}-${part2 + "X".repeat(5-(part2?.length as number))}-${part3 + "X".repeat(1-(part3?.length as number))}`
 }
+export function dateCountdown(date: Date) {
+	const diff = dateDiff(date, new Date())
+	// just use new Date(diff).year ????
+	const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365))
+	const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30))
+	const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+	const hours = Math.floor(diff / (1000 * 60 * 60))
+	const minutes = Math.floor(diff / (1000 * 60))
+	const seconds = Math.floor(diff / 1000)
+	return { years, months, days, hours, minutes, seconds }
+}
+export function convertToDate(date: string | undefined) {
+	// no idea what it does
+	if (!date) {
+		return
+	}
+	return new Date(date)
+}
+export function dateToString(date: Date) {
+	return date.toISOString()
+}
+export function dateDiff(date1: Date, date2: Date) {
+	return date1.getTime() - date2.getTime()
+}
 export function convertDiffToText(diff:number) {
 	if (diff >= 60*60*1000) {
 		return `${Math.round(diff/(60*60*1000))} hours ago`
