bf4831ce74525104c273b1276a9762be215782d8
phasenull <55253035+phasenull@users.noreply.github.com>
2024-06-30 19:40:50 +0300

    useGetUser mutation

--- src/common/hooks/fkart/auth/useGetUser.ts
+++ src/common/hooks/fkart/auth/useGetUser.ts
@@ -1,43 +1,30 @@
-import { useQuery } from "react-query"
-import axios, { AxiosResponse } from "axios"
-import BaseFKartResponse from "common/interfaces/FKart/BaseFKartResponse"
-import Logger from "common/Logger"
-import ApplicationConfig from "common/ApplicationConfig"
-import FKartUser from "common/interfaces/FKart/FKartUser"
-import ICredentials from "common/interfaces/app/Credentials"
-import { ToastAndroid } from "react-native"
+import { useMutation } from "react-query"
+import { useFKartAuthStore } from "common/stores/FKartAuthStore"
 
-async function getUser(credentials?: ICredentials): Promise<
-	| AxiosResponse<
-			| BaseFKartResponse & {
-					session?: { user: FKartUser; refresh_token: string; is_2fa_enabled: boolean }
-					twoFA_session_id?: string
-			  }
-	  >
-	| undefined
-> {
-	Logger.info("REQUEST getUser")
-	return axios(`${ApplicationConfig.fkart_endpoints.auth}/user/get`, {
-		headers:{
-			"User-Agent":ApplicationConfig.getFormattedVersion()
-		},
-		method: "POST",
-		data: JSON.stringify({
-			email: credentials?.username,
-			password: credentials?.password,
-			twoFA_session_id: credentials?.twoFA_session,
-			twoFA_code: credentials?.twoFA_code,
-		}),
-	})
-}
+export default function useGetUser() {
+	const login = useFKartAuthStore((state) => state.login)
+	return useMutation(
+		["getUser"],
+		(credentials: { username?: string; password?: string; twoFA_session_id?: string; twoFA_code?: string }) => {
+			// cursed promise
+			const promise = new Promise(async (resolve, reject) => {
+				if (!credentials.username) return reject("Username field cannot be empty!")
+				if (!credentials.password) return reject("Password field cannot be empty!")
 
-export default function useGetUser(credentials?: ICredentials) {
-	return useQuery(["pushUser"], () => getUser(credentials), {
-		enabled: false,
-		retry: false,
-		useErrorBoundary: false,
-		onError: (e: any) => {
-			// ToastAndroid.show(`error ${e?.response?.data?.result?.error}`,1000)
+				const [success, error] = await login({
+					username: credentials.username,
+					password: credentials.password,
+					twoFA_code: credentials.twoFA_code,
+				})
+				if (!success) {
+					return reject(error)
+				}
+				return resolve(success)
+			})
+			return promise
 		},
-	})
+		{
+			retry: false,
+		}
+	)
 }
